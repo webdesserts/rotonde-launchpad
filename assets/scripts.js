ce84e8a55793d45b8414ab6f020ad3d63ee1a864
Michael Mullins <michael@webdesserts.com>
2017-11-11 01:53:57 -0600

    implement custom avatar uploads

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,9 +11,14 @@ $liftoff.addEventListener('click', liftoff)
 // pre download the ref implementation so its ready when they fork
 const neauoire_url = "dat://2f21e3c122ef0f2555d3a99497710cd875c7b0383f998a2d37c02c042d598485/"
 const neauoire_archive = new DatArchive(neauoire_url)
+const state = {
+  avatar_image: null
+}
+
 neauoire_archive.download()
 
 initPanes()
+initAvatar()
 
 function initPanes () {
   const $active = document.querySelector('.pane-view > .pane--active')
@@ -84,7 +89,8 @@ async function liftoff () {
 
   await portal.writeFile('/portal.json', JSON.stringify(portal_str));
 
-  let icon = await launchpad.readFile('/assets/icon.svg')
+  let icon = state.avatar_image;
+  if (!icon) { icon = await launchpad.readFile('/assets/icon.svg') }
   await portal.writeFile('/media/content/icon.svg', icon);
 
   await portal.commit();
@@ -97,5 +103,46 @@ async function cleanArchive(archive) {
 
   for (const file of frozen_files) {
     await archive.unlink(file)
+  }
+}
+
+function initAvatar($input) {
+  const $avatar = document.querySelector('.field.avatar input[type="file"]')
+  $avatar.addEventListener('change', async () => {
+    const file = $avatar.files[0]
+    if (!validateAvatar(file)) { return; }
+    const svg = await readAvatar(file)
+    const $image = document.querySelector('.field.avatar img')
+    $image.src = svgToUrl(svg)
+    state.avatar_image = svg
+  })
+}
+
+// Am I doing this right??
+function readAvatar(file) {
+  let reader = new FileReader()
+  reader.readAsText(file)
+  return new Promise((resolve, reject) => {
+    reader.addEventListener('load', () => {
+      resolve(reader.result)
+    })
+    reader.addEventListener('error', () => {
+      reject(reader.error)
+    })
+  })
+}
+
+function svgToUrl(string) {
+  return `data:image/svg+xml;base64,${window.btoa(string)}`
+}
+
+function validateAvatar(file) {
+  const $field = document.querySelector('.field.avatar')
+  if (file.type == "image/svg+xml") {
+    $field.removeAttribute('data-invalid')
+    return true
+  } else {
+    $field.setAttribute('data-invalid', "Image must be an svg")
+    return false
   }
 }
